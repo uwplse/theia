53e27e4fc8614dfc3b7fd1cf66eb8843602ddda1
Josh Pollock <joshpoll@cs.washington.edu>
2019-05-20 13:41:02 -0700

    parse lets

--- parse-log.py
+++ parse-log.py
@@ -1,19 +1,24 @@
 #!/usr/local/bin/python3
 
+import os
+
 test_file = "test.log"
+dir_path = "semantics/lambda/debugger-output/lets"
 
 def main():
     with open(test_file, "r") as f:
-        # read log file
-        data = f.read()
-        print(len(data.split("KDebug> 1 Step(s) Taken.\nKDebug> p\ns")))
+        # read, clean, and separate log file
+        entries = f.read() \
+                   .split("KDebug> p\ns\n", 1)[-1] \
+                   .split("KDebug> Attempted 1 step(s). Took 0 steps(s).\nFinal State Reached", 1)[0] \
+                   .split("KDebug> 1 Step(s) Taken.\nKDebug> p\ns")
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
 
-# split on
-'''
-KDebug> 1 Step(s) Taken.
-KDebug> p
-s
-'''
+        for (i, e) in enumerate(entries):
+            with open(dir_path + "/" + str(i) + ".json", 'w') as e_file:
+                e_file.write(e)
+
 # write the separate parts to separate files
 # (eventually) print number of files or write that to a separate file somewhere
 
